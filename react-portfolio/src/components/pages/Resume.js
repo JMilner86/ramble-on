954454e3291c763fd9a46d11d0ef273cdb61b6e6
Jonah Milner <jonahmilner86@gmail.com>
2022-06-11 14:52:01 -0600

    added pdf of my resume

--- react-portfolio/src/components/pages/Resume.js
+++ react-portfolio/src/components/pages/Resume.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import pdf from '../../pdf/Jonah Milner resume.pdf';
 
 export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
+      <div>
+      <a href={pdf} target="_blank" rel="noreferrer">
+        My Resume pdf
+      </a>
+    </div>
+
     <ul>
         <h2>Proficiencies</h2>
         <li><h3>JavaScript</h3></li>
